abbacd3a074eb17c224c79f3111e3231197c880c
DanielCigarrero <147728487+DanielCigarrero@users.noreply.github.com>
2024-03-04 14:52:43 +0000

    :D
    :D

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 // Declare variables below to save the different divs of your story.
 
+let storyOpening = document.querySelector(".story-opening")
+
+let optionOneButton = document.querySelector(".option-one")
+let optionOneScreen = document.querySelector(".option-one-screen");
+let optionOneEnd = document.querySelector(".option-one-end")
+
+let optionTwoButton = document.querySelector(".option-two")
+let optionTwoScreen = document.querySelector(".option-two-screen")
+let optionTwoEnd = document.querySelector(".option-two-end")
 
 
+optionOneButton.addEventListener('click', function() {
+optionOneScreen.computedStyleMap.display='block';
+storyOpening.style.display='none';
 
-
-
+  
+});
 
 
 
